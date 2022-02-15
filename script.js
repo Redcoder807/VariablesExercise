b3dccbdf53ace5c6d862d5f32c46b0bd7fa2c9ae
Redcoder807 <gearonhaynes@gmail.com>
2022-02-15 10:16:39 -0600

    Bonus

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 // const
+
 let num = 20;
+
 num = 15;
+
 num = num +7;
+
 num--;
+
 num = "This is not a number";
+
 let blank;
+
 console.log(blank);
-blank = "";
+
+blank = null;
+
 console.log(blank);
+
+// Bonus
+
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+// const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01]
+// const quarter = 0.25; const dime = 0.10; const nickel = 0.05; const penny = 0.01;
+
+// console.log(quarter +" " + dime + " " + nickel + " " + penny);
+console.log(quarter, dime, nickel, penny);
+
+let letters = "qwertyuiop";
+
+console.log(letters[letters.length - 1 ]);
